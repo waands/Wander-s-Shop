bdf8f543935412134086a44dfda0568a1c8d4ffb
waands <wanderjunior36@gmail.com>
2025-02-10 22:11:28 -0300

    lógica de casos de uso do carrinho (adicionar remover items + alterar a quantidade dos itens) e integração do carrinho com o sistema

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Product } from "src/app/models/product.model";
 
 @Component({
-  selector: 'app-product-box',
-  templateUrl: './product-box.component.html',
+  selector: "app-product-box",
+  templateUrl: "./product-box.component.html",
 })
-
 export class ProductBoxComponent implements OnInit {
+  @Input() fullWidthMode = false;
+  product: Product | undefined = {
+    id: 1,
+    title: "Botas Mágicas",
+    price: 150,
+    category: "Sapatos",
+    description: "Descrição do produto",
+    image: "https://via.placeholder.com/150",
+  };
+  @Output() addToCart = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onAddToCart(): void {
+    this.addToCart.emit(this.product);
   }
-
 }
