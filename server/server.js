4dc8e3ab297510ca1e93192d08d6f9808b150635
waands <wanderjunior36@gmail.com>
2025-02-13 16:20:26 -0300

    shipping option added

--- server/server.js
+++ server/server.js
@@ -1,39 +1,85 @@
-const express = require('express');
-const cors = require('cors');
-const bodyparser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyparser = require("body-parser");
 
 const app = express();
 app.use(express.static("public"));
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
-app.use(cors({origin: true, credentials: true}));
+app.use(cors({ origin: true, credentials: true }));
 
-const stripe = require("stripe")("sk_test_51QoXFXPg7IpyOK8rNzeyf5F1yGy0ZHEcHvbXYBO5qpXjLSifqdo3o1job6MOumIP66DCrnOOraTuVuiba4ZrRZnG00C99vj0bv");
+const stripe = require("stripe")(
+  "sk_test_51QoXFXPg7IpyOK8rNzeyf5F1yGy0ZHEcHvbXYBO5qpXjLSifqdo3o1job6MOumIP66DCrnOOraTuVuiba4ZrRZnG00C99vj0bv"
+);
 
 app.post("/checkout", async (req, res, next) => {
-    try {
-        const session = await stripe.checkout.sessions.create({
-            line_items: req.body.items.map((item) => ({
-                price_data: {
-                    currency: "brl",
-                    product_data: {
-                        name: item.name,
-                        images: [item.image],
-                    },
-                    unit_amount: item.price * 100,
-                },
-                quantity: item.quantity
-            })),
-            mode: "payment",
-            success_url: "http://localhost:4242/success.html",
-            cancel_url: "http://localhost:4242/cancel.html",
-        });
+  try {
+    const session = await stripe.checkout.sessions.create({
+      shipping_address_collection: {
+        allowed_countries: ["BR", "AR"],
+      },
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: 0,
+              currency: "brl",
+            },
+            display_name: "Free shipping",
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 5,
+              },
+              maximum: {
+                unit: "business_day",
+                value: 7,
+              },
+            },
+          },
+        },
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: 1500,
+              currency: "brl",
+            },
+            display_name: "Next day air",
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 1,
+              },
+              maximum: {
+                unit: "business_day",
+                value: 1,
+              },
+            },
+          },
+        },
+      ],
+      line_items: req.body.items.map((item) => ({
+        price_data: {
+          currency: "brl",
+          product_data: {
+            name: item.name,
+            images: [item.image],
+          },
+          unit_amount: item.price * 100,
+        },
+        quantity: item.quantity,
+      })),
+      mode: "payment",
+      success_url: "http://localhost:4242/success.html",
+      cancel_url: "http://localhost:4242/cancel.html",
+    });
 
-        res.status(200).json(session);
-    } catch (error) {
-        next(error);
-    }
+    res.status(200).json(session);
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.listen(4242, () => console.log("Server is running on port 4242"));
-
