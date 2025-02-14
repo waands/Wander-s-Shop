b9a5ff19c7c24919416ab55b07ec041de725dbac
waands <wanderjunior36@gmail.com>
2025-02-14 12:51:57 -0300

    fix: correct templateUrl property in success and cancel components; update tslib version

--- src/app/pages/success/success.component.ts
+++ src/app/pages/success/success.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-success',
-  templateURL: './success.component.html',
+  templateUrl: './success.component.html',
 })
 export class SuccessComponent implements OnInit {
 
